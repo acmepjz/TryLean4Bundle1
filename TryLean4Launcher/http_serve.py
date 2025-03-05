531a431694308830e0d32008b209165c9f161bd1
acme_pjz <acme_pjz@hotmail.com>
2025-03-05 21:29:47 +0800

    add a launcher (experimental)

---
+++ TryLean4Launcher/http_serve.py
@@ -0,0 +1,93 @@
+import io
+import zipfile
+import zlib
+import threading
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from urllib.parse import unquote
+
+# Path to the zip file
+ZIP_FILE_PATH = 'doc.zip'
+
+class InMemoryZipHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.zip_file = None
+        super().__init__(*args, **kwargs)
+
+    def load_zip(self):
+        if self.zip_file is None:
+            self.zip_file = zipfile.ZipFile(ZIP_FILE_PATH, 'r')
+
+    def send_response_from_zip(self, file_name):
+        self.load_zip()
+        file_name = unquote(file_name.lstrip('/'))
+        try:
+            file_name_br = file_name + ".br"
+            file_data = self.zip_file.read(file_name_br)
+            crc32 = self.zip_file.getinfo(file_name_br).CRC  # Get the CRC-32 value from the ZIP file info
+            content_encoding = "br"
+        except KeyError:
+            try:
+                file_data = self.zip_file.read(file_name)
+                crc32 = self.zip_file.getinfo(file_name).CRC  # Get the CRC-32 value from the ZIP file info
+                content_encoding = None
+            except KeyError:
+                self.send_error(404, "File not found")
+                return
+        
+        # Check If-None-Match header from the browser
+        if 'If-None-Match' in self.headers and self.headers['If-None-Match'] == str(crc32):
+            self.send_response(304)  # Not Modified
+            self.end_headers()
+            return
+
+        # Send 200 OK with file data
+        self.send_response(200)
+        self.send_header("Content-type", self.guess_type(file_name))
+        self.send_header("Content-length", len(file_data))
+        self.send_header("ETag", str(crc32))  # Send CRC as ETag
+        self.send_header("Cache-Control", "max-age=3600")  # Cache for 1 hour
+        if not content_encoding is None:
+            self.send_header("Content-Encoding", content_encoding)
+        self.end_headers()
+        self.wfile.write(file_data)
+
+    def do_GET(self):
+        """Serve a file from the zip archive"""
+        # Normalize path to ensure no double slashes
+        path = self.path.replace('//', '/')
+        while '//' in path:
+            path = path.replace('//', '/')
+        
+        if path.startswith('/'):
+            file_name = path
+            self.send_response_from_zip(file_name)
+        else:
+            super().do_GET()
+
+class ServerThread(threading.Thread):
+    def __init__(self, server_address, handler_class):
+        super().__init__()
+        self.server_address = server_address
+        self.handler_class = handler_class
+        self.httpd = HTTPServer(self.server_address, self.handler_class)
+        self.running = True
+
+    def run(self):
+        print(f"Server started on {self.server_address}")
+        self.httpd.serve_forever()
+
+    def stop(self):
+        self.httpd.shutdown()
+        self.httpd.server_close()
+        self.running = False
+        print("Server stopped.")
+
+def run(handler_class=InMemoryZipHTTPRequestHandler, port=13480):
+    print(f"Starting server on port {port}...")
+    server_address = ('127.0.0.1', port)
+    httpd = HTTPServer(server_address, handler_class)
+    print(f"Serving files from zip archive '{ZIP_FILE_PATH}'")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
---
+++ TryLean4Launcher/main.py
@@ -0,0 +1,144 @@
+import gettext, locale, os, os.path
+import zipfile
+import tkinter as tk
+from tkinter import messagebox, ttk
+from time import sleep
+import http_serve
+from http_serve import InMemoryZipHTTPRequestHandler, ServerThread
+
+def init_locale():
+    for key in ["LANGUAGE", "LC_ALL", "LC_MESSAGES", "LANG"]:
+        if key in os.environ:
+            return True
+    def_locale = locale.getdefaultlocale()[0]
+    os.environ["LANG"] = def_locale
+
+init_locale()
+gettext.install('messages', 'locale')
+
+class MainWindow:
+    def __init__(self, root):
+        self.root = root
+        self.server_thread = None
+        self.has_cache = False
+
+        root.title(_("Try Lean4 Windows Bundle Launcher"))
+
+        mainframe = ttk.Frame(root)
+        mainframe.pack(fill=tk.BOTH, expand=True, padx=8, pady=(8, 4))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(1, weight=1)
+        mainframe.rowconfigure(3, weight=1)
+        mainframe.rowconfigure(5, weight=1)
+
+        tk.Label(mainframe, text=_("Unpack mathlib cache"), bg="gray", fg="white", anchor="w").grid(column=0, row=0, sticky="news")
+
+        frame = ttk.Frame(mainframe)
+        frame.grid(column=0, row=1, sticky="news", pady=4)
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
+        lf = ttk.Labelframe(frame, text=_('mathlib cache status'))
+        lf.grid(column=0, row=0, sticky="news")
+        lf.rowconfigure(0, weight=1)
+        lf.columnconfigure(0, weight=1)
+        self.cache_status = tk.StringVar()
+        tk.Label(lf, textvariable=self.cache_status).grid(column=0, row=0, sticky="news")
+        ttk.Button(frame, text=_("Unpack mathlib cache"), command=self.unpack_cache).grid(column=1, row=0, sticky="news", padx=(4, 0))
+        self.check_cache_status()
+
+        tk.Label(mainframe, text=_("Start Try Lean4 Windows Bundle"), bg="gray", fg="white", anchor="w").grid(column=0, row=2, sticky="news")
+
+        frame = ttk.Frame(mainframe)
+        frame.grid(column=0, row=3, sticky="news", pady=4)
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
+        ttk.Button(frame, text=_("Start Lean4 VSCode code editor"), command=self.start_vscode).grid(column=0, row=0, sticky="news")
+        ttk.Button(frame, text=_("Start Lean4 bash command line"), command=self.start_bash).grid(column=1, row=0, sticky="news", padx=(4, 0))
+
+        tk.Label(mainframe, text=_("Offline mathlib help"), bg="gray", fg="white", anchor="w").grid(column=0, row=4, sticky="news")
+
+        frame = ttk.Frame(mainframe)
+        frame.grid(column=0, row=5, sticky="news", pady=4)
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
+        self.start_button = ttk.Button(frame, text=_("Start offline mathlib help server"), command=self.start_server)
+        self.start_button.grid(column=0, row=0, sticky="news")
+        self.stop_button = ttk.Button(frame, text=_("Stop offline mathlib help server"), command=self.stop_server, state=tk.DISABLED)
+        self.stop_button.grid(column=1, row=0, sticky="news", padx=(4, 0))
+        self.browse_button = ttk.Button(frame, text=_("Open offline mathlib help"), state=tk.DISABLED, command=self.start_browser)
+        self.browse_button.grid(column=2, row=0, sticky="news", padx=(4, 0))
+        
+        root.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def start_server(self):
+        if self.server_thread is None or not self.server_thread.is_alive():
+            try:
+                tmp = zipfile.ZipFile(http_serve.ZIP_FILE_PATH, 'r')
+            except:
+                messagebox.showerror(title=_("Error"), message=_("Failed to load file %s") % http_serve.ZIP_FILE_PATH)
+                return
+
+            self.server_thread = ServerThread(('127.0.0.1', 13480), InMemoryZipHTTPRequestHandler)
+            self.server_thread.start()
+
+            self.start_button.config(state=tk.DISABLED)
+            self.stop_button.config(state=tk.NORMAL)
+            self.browse_button.config(state=tk.NORMAL)
+
+            print("Server is starting...")
+            
+            sleep(0.1)
+            
+            self.start_browser()
+
+    def stop_server(self):
+        if self.server_thread is not None:
+            self.server_thread.stop()
+            self.start_button.config(state=tk.NORMAL)
+            self.stop_button.config(state=tk.DISABLED)
+            self.browse_button.config(state=tk.DISABLED)
+
+            print("Server is stopping...")
+
+    def start_browser(self):
+        os.startfile("http://127.0.0.1:13480/doc/index.html")
+
+    def unpack_cache(self):
+        if self.has_cache:
+            if messagebox.askquestion(title=_("Mathlib cache already exists"), message=_("Mathlib cache already exists. Do you want to reinstall?")) != messagebox.YES:
+                return
+        os.system("start /wait cmd /c unpack_cache.cmd")
+        sleep(0.1)
+        self.check_cache_status()
+
+    def start_vscode(self):
+        os.startfile("start_Lean_VSCode.cmd")
+
+    def start_bash(self):
+        os.startfile("start_Lean_bash.cmd")
+
+    def on_close(self):
+        if self.server_thread is not None:
+            self.server_thread.stop()
+        self.root.destroy()
+    
+    def check_cache_status(self):
+        self.has_cache = False
+        self.cache_status.set(_("Not installed"))
+        try:
+            if os.path.isfile("projects/LeanPlayground/.lake/packages/mathlib/.lake/build/lib/Mathlib/Init.olean"):
+                self.has_cache = True
+                self.cache_status.set(_("Installed"))
+        except:
+            pass
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    MainWindow(root)
+    root.mainloop()
