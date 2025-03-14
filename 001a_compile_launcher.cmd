b3c933066532768b3df712fe78238f3fe59af251
acme_pjz <acme_pjz@hotmail.com>
2025-03-15 01:37:13 +0800

    adapt build script

--- 001a_compile_launcher.cmd
+++ 001a_compile_launcher.cmd
@@ -1,27 +1,10 @@
-:: install python
+:: copy files
 
-curl --retry 5 -L -o "Downloads\Winpython64-3.13.2.0dot.7z" "https://github.com/winpython/winpython/releases/download/13.1.202502222final/Winpython64-3.13.2.0dot.7z"
-Downloads\7zr.exe x "Downloads\Winpython64-3.13.2.0dot.7z" -o"Downloads"
+robocopy TryLean4Launcher TryLean4Bundle\TryLean4Launcher /S /NFL /NDL /NP
 
-set PATH=%CD%\Downloads\WPy64-31320\python;%CD%\Downloads\WPy64-31320\python\Scripts;%PATH%
-set PYTHON=%CD%\Downloads\WPy64-31320\python\python.exe
+:: create script
 
-:: install pyinstaller
-
-pip install pyinstaller
-
-:: compile launcher
-
-cd TryLean4Launcher
-
-pyinstaller TryLean4Launcher.py
-
-:: move exe
-
-cd ..
-move /y TryLean4Launcher\dist\TryLean4Launcher\TryLean4Launcher.exe TryLean4Bundle\
-move /y TryLean4Launcher\dist\TryLean4Launcher\_internal TryLean4Bundle\
-robocopy TryLean4Launcher\locale TryLean4Bundle\locale /S /NFL /NDL /NP
+echo powershell -ExecutionPolicy Bypass -File TryLean4Launcher\TryLean4Launcher.ps1>TryLean4Bundle\TryLean4Launcher.cmd
 
 :: TODO error handle
 
