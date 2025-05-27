f0808488b5f183b9f79867313e43b47fa5996c2f
acme_pjz <acme_pjz@hotmail.com>
2025-05-27 18:47:30 +0800

    change git commit time detection code

--- Resources/check_mathlib_doc_version.ps1
+++ Resources/check_mathlib_doc_version.ps1
@@ -3,10 +3,14 @@ try {
 	$A = $A[0].Matches[0].Groups[1].Value
 	Out-File -FilePath .\doc_version.txt -InputObject $A -Encoding ascii
 	try {
-		$B = & gh api "/repos/leanprover-community/mathlib4/commits/$A" --jq ".commit.author.date"
+		# Somehow this is not working... Let's just download the commit page on github.
+		# $B = & gh api "/repos/leanprover-community/mathlib4/commits/$A" --jq ".commit.author.date"
+		$Response = Invoke-WebRequest -Uri "https://github.com/leanprover-community/mathlib4/commit/$A"
+		$B = $Response.Content | Select-String -Pattern '"committedDate":"([^"]*)"'
+		$B = $B[0].Matches[0].Groups[1].Value
 		Out-File -FilePath .\doc_version.txt -Append -InputObject $B -Encoding ascii
 	} catch {
-		Write-Host "::warning::failed to get git commit time from git commit hash (gh not installed?)"
+		Write-Host "::warning::failed to get git commit time from git commit hash"
 	}
 } catch {
 	Write-Host "::warning::failed to get mathlib doc version"
